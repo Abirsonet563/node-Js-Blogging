00e446506002450f0b746583fe6fd543e0cccb71
Abir Hasan <abirsonet563@gmail.com>
2024-12-07 22:04:18 +0000

    Delete File

--- file.js
+++ file.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
 
-//directories
-if (! fs.existsSync('./assets')){
-fs.mkdir('./assets', (err) => {
-    if(err){
-        console.log(err);
-    }
-    console.log("folder Created");
-})} else{
-    fs.rmdir('./assets', (err) => {
-        if(err) {
+//Delete
+if (fs.existsSync('./deleteMe.txt')){
+    fs.unlink('./deleteMe.txt', (err) => {
+        if (err) {
             console.log(err)
         }
-        console.log('Folder Deleted')
+        
     })
+    console.log("Deleted")
 }
