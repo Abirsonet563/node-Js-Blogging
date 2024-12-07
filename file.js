7967c25974f7ed3c4267a13fb3665136a5b10d3e
Abir Hasan <abirsonet563@gmail.com>
2024-12-07 21:17:30 +0000

    Read File

---
+++ file.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+//read file
+fs.readFile('./docs/blog1.txt', (err, data) => {
+    if(err){
+        console.log(err);
+    }else{
+    console.log(data.toString());
+    }
+});
