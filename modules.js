7ce50bd991f9ce2e6019ef692c7ec32bb54fd705
Abir Hasan <abirsonet563@gmail.com>
2024-12-07 21:08:34 +0000

    First Commit

---
+++ modules.js
@@ -0,0 +1,9 @@
+const peoples = ["abir", "hasan"];
+const age = ["1abir", "2hasan"];
+
+console.log(peoples);
+
+
+module.exports = {
+    age, peoples
+};
---
+++ people.js
@@ -0,0 +1,7 @@
+const {peoples, age} = require('./modules.js')
+console.log(peoples);
+
+console.log(peoples, age);
+
+const os = require('os');
+console.log(os.platform());
