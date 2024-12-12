2cd55a3634e71911937abaab43efe967c6a03780
Abir Hasan <abirsonet563@gmail.com>
2024-12-12 01:00:54 +0000

    response object

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<head><link/ rel="stylesheet" href="#"></head>')
+  res.write('<p> hello Ninja </p>')
+  res.write('<p> Hello EveryOne </p>')
+  res.end();
 });
 
 server.listen(3000, 'localhost', ()=> {
