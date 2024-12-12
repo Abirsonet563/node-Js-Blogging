bea040cf0b456c283f23f0f7035dff77b3bcff42
Abir Hasan <abirsonet563@gmail.com>
2024-12-12 01:16:27 +0000

    Returing HTML Page

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
+  //set Header Content type
   res.setHeader('Content-Type', 'text/html');
-  res.write('<head><link/ rel="stylesheet" href="#"></head>')
-  res.write('<p> hello Ninja </p>')
-  res.write('<p> Hello EveryOne </p>')
-  res.end();
+
+  fs.readFile('./view/index.html', (err, data) => {
+    if(err){
+        console.log(err);
+        res.end();
+    }else{
+        // res.write(data);
+        res.end(data);
+    }
+  })
 });
 
 server.listen(3000, 'localhost', ()=> {
