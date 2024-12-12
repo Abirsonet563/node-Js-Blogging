cca5f965b4f136b01e5d49d517a6e0bdd653c524
Abir Hasan <abirsonet563@gmail.com>
2024-12-12 02:27:07 +0000

    Basic Routing with Switch Case

--- server.js
+++ server.js
@@ -7,15 +7,26 @@ const server = http.createServer((req, res) => {
   //set Header Content type
   res.setHeader('Content-Type', 'text/html');
 
-  fs.readFile('./view/index.html', (err, data) => {
-    if(err){
-        console.log(err);
-        res.end();
-    }else{
-        // res.write(data);
-        res.end(data);
-    }
-  })
+ let path = "./view/"
+ switch(req.url){
+      case '/':
+        path += "/index.html";
+        break;
+      case '/about':
+        path += "about.html"
+        break;
+      default: path += "404.html";
+      break;
+ }
+
+ fs.readFile(path, (err, data) => {
+  if(err){
+    console.log(err);
+    res.end();
+  }else{
+    res.end(data);
+  }
+ })
 });
 
 server.listen(3000, 'localhost', ()=> {
