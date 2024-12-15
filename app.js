21979e3ba21f1f907412d9e453f66839010f0822
Abir Hasan <abirsonet563@gmail.com>
2024-12-15 23:29:46 +0000

    Redirect & 404 Page

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
      // res.send('<p>Home Page</p>');
-     res.sendFile('./view/index.html', {root: __dirname})
-})
+     res.sendFile('./view/index.html', {root: __dirname});
+});
 
 app.get('/about', (req, res) => {
      // res.send('about page')
-     res.sendFile('./view/about.html', {root: __dirname})
+     res.sendFile('./view/about.html', {root: __dirname});
+});
+
+//redirect
+
+app.get('/about-us', (req, res) => {
+     res.redirect('/about');
+});
+
+//404 Page
+app.use((req, res) => {
+     res.status(404).sendFile('./view/404.html', {root: __dirname});
 })
